a7e6c2a191f008541b01b8f4ea8f7d3784dcb4d1
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-01-30 23:19:38 +0530

    Time: 3 ms (38.83%), Space: 6.2 MB (22.73%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-   int t0=0, t1=1, t2=1;
-    long long int arr[38]={0};
-    int tribonacci(int n) {
-        
-        if(n==0)
-            return t0;
-        if(n==1)
-            return t1;
-        if(n==2)
-            return t2;
-        if(arr[n]!=0)
-            return arr[n];
-        return arr[n]=tribonacci(n-3)+tribonacci(n-2)+tribonacci(n-1);
+    int func(vector<int> &dp,int n){
+        if(n==0) return 0;
+        if(n==1||n==2) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=func(dp,n-1)+func(dp,n-2)+func(dp,n-3);
+    }
+    int tribonacci(int n){
+
+        vector<int> dp(n+1,-1);
+        return func(dp,n);
     }
 };
