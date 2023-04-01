db15cf1d151aea48fd00dfc4f7c58866472bf5df
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-04-01 23:49:33 +0530

    Time: 7 ms (71.07%), Space: 14.7 MB (86.51%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,27 +11,21 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-         //first see if any of the two given list is null  or not
- if(list1 == NULL)return list2;
- if(list2 == NULL)return list1;
- 
- //find the pointer having least value
- if(list1->val > list2->val)swap(list1,list2);
- //initialize new pointer to the smallest pointer val
- ListNode* res=list1;
- //now iterate over l1 and l2;
- while(list1 != NULL && list2 != NULL){
-     //node for connecting
-     ListNode* temp=NULL;
-     //iterate over l1 till l1<=l2
-     while(list1 !=NULL and list1 ->val <= list2->val){
-         temp=list1;
-         list1=list1->next;
-     }
-     //when l2<l1
-     temp->next=list2;
-     swap(list1, list2);
- }
- return res;
+        ListNode* dummy=new ListNode(0);
+        ListNode* cur=dummy;
+        while(list1&&list2){
+            if(list1->val<=list2->val){
+                cur->next=list1;
+                list1=list1->next;
+            }
+            else{
+                cur->next=list2;
+                list2=list2->next;
+            }
+            cur=cur->next;
+        }
+        cur->next=(list1==NULL)?list2:list1;
+        return dummy->next;
+        
     }
 };
