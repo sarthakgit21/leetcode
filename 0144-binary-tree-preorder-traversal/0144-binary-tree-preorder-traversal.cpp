ebb6934627b3f414022203f691b10d4c48c4d71f
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-04-03 22:27:48 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (55.57%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,26 +1,52 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    void fun(vector<int> &nodes,TreeNode* root){
-        if(!root) return ; 
-        nodes.push_back(root->val);
-        fun(nodes,root->left);
-        fun(nodes,root->right);
-    }
-    
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> nodes;
-        fun(nodes,root);
-        return nodes;
+class Solution
+{
+    public:
+        void fun(vector<int> &nodes, TreeNode *root)
+        {
+            if (!root) return;
+            nodes.push_back(root->val);
+            fun(nodes, root->left);
+            fun(nodes, root->right);
+        }
+
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> ans;
+        while (root)
+        {
+            if (!root->left)
+            {
+                ans.push_back(root->val);
+                root = root->right;
+            }
+            else
+            {
+                TreeNode *cur = root->left;
+                while (cur->right && cur->right != root) cur = cur->right;
+                if (cur->right == NULL)
+                {
+                    cur->right = root;
+                    ans.push_back(root->val);
+                    root = root->left;
+                }
+                else
+                {
+                    cur->right = NULL;
+                    root = root->right;
+                }
+            }
+        }
+        return ans;
     }
 };
