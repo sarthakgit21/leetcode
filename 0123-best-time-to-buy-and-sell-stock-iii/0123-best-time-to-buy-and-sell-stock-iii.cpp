2d13942bd6780921deafa9d100673bac5ea656e8
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-04-09 00:55:03 +0530

    Time: 339 ms (57.41%), Space: 135.2 MB (54.54%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,25 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int  n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int> (3,0)));
-        for(int i=n-1;i>=0;i--)
+class Solution
+{
+    public:
+        int maxProfit(vector<int> &prices)
         {
-            for(int buy=0;buy<=1;buy++)
+            int n = prices.size();
+            vector<vector < int>> dp(n + 1, vector<int> (5, 0));
+            for (int i = n - 1; i >= 0; i--)
             {
-                for(int cap=1;cap<=2;cap++){
-                    if(buy){
-                        dp[i][buy][cap]=max(-prices[i]+dp[i+1][0][cap],dp[i+1][1][cap]);
+                for (int t = 0; t < 4; t++)
+                {
+                    if (t %2==0)
+                    {
+                        dp[i][t] = max(-prices[i] + dp[i + 1][t + 1], dp[i + 1][t]);
                     }
-                    else{
-                        dp[i][buy][cap]=max(prices[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);
+                    else
+                    {
+                        dp[i][t] = max(prices[i] + dp[i + 1][t + 1], dp[i + 1][t]);
                     }
                 }
             }
+            return dp[0][0];
+            
         }
-        return dp[0][1][2];
-        
-    }
 };
