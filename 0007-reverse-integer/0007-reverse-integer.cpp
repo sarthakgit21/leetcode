255c7f0a5481cc637eccc83087ee3ab519fa5d17
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-04-25 22:44:20 +0530

    Update 0007-reverse-integer.cpp

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int reverse(int x) {
-        int ans=0;
+        int ans=0,a=0;
         while(x){
             if(ans>(INT_MAX)/10||ans<(INT_MIN)/10) return 0;
             else{
