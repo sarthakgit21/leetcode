0ef535d35f4b70496df5db13d157c4ae7c251844
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-06-03 10:22:08 +0530

    Time: 318 ms (65.94%), Space: 121.9 MB (59.75%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>>&tree,int node,vector<int>& informTime) {
+        if(tree[node].size()==0) return 0;
+        int currMax = 0;
+        for(int i=0;i<tree[node].size();i++) currMax = max(currMax,informTime[node] + dfs(tree,tree[node][i],informTime));
+        return currMax;
+    }
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        //finding the maximum depth of the tree will be our answer.
+        //but how to perform bfs or dfs on an array?
+        vector<vector<int>>tree(n);
+        int ans = 0;
+        for(int i=0;i<n;i++) if(manager[i]!=-1) tree[manager[i]].push_back(i);
+        for(int i=0;i<tree[headID].size();i++) ans = max(ans,informTime[headID] + dfs(tree,tree[headID][i],informTime));
+        return ans;
+    }
+};
