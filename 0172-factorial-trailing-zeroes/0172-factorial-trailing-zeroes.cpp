6ba2e4f9914d98be980a9f6e239931d692ffd0b7
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-08-22 18:38:02 +0530

    Time: 3 ms (40.83%), Space: 5.9 MB (89.60%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -3,22 +3,10 @@ public:
     int trailingZeroes(int n) {
         int ans=0;
         
-        for(int i=5;i*5<=n;i+=5)
-        {
-            int val=i*5;
-            int num_of_fives=0;
-            while(val%5==0)
-            {
-                val/=5;
-                num_of_fives++;
-            }
-            num_of_fives--;
-            // cout<<i*5<<" "<<num_of_fives<<endl;
-            ans+=num_of_fives;
-           
-            
+        while(n>0){
+            ans+=n/5;
+            n/=5;
         }
-        ans+=n/5;
         return ans;
         
     }
