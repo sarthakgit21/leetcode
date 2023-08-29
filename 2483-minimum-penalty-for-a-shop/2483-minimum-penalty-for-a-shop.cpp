971b1eff6c005de6a21e8c36eb3754bad785f291
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-08-29 14:46:58 +0530

    Time: 21 ms (87.90%), Space: 10.6 MB (86.10%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int ans=INT_MAX;
-        int maxi=INT_MAX;
+        int ans=0;
+        int ind=-1;
         int n=customers.size();
-        vector<int> left(n+1,0);
-        vector<int> right(n+1,0);
-        if(customers[0]=='Y') left[0]=0;
-        else left[0]=1;
-        for(int i=1;i<=n;i++)
+        int score=0;
+        for(int i=0;i<n;i++)
         {
-            left[i]=left[i-1]+(customers[i]=='N'?1:0);
-        }
-        right[n]=0;
-        if(customers[n-1]=='Y') right[n-1]=1;
-        else right[n-1]=0;
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            right[i]=right[i+1]+(customers[i]=='Y'?1:0);
-        }
-        // cout<<right[0];
-         maxi=right[0];
-        ans=0;
-        
-//         for(int i=0;i<=n;i++)
-//         {
-//             cout<<left[i]<<" ";
-//         }
-//         cout<<endl;
-        
-//         for(int i=0;i<=n;i++)
-//         {
-//             cout<<right[i]<<" ";
-//         }
-        for(int i=1;i<=n;i++)
-        {
-            if(right[i]+left[i-1]<maxi)
+            if(customers[i]=='Y') score++;
+            else score--;
+            if(score>ans)
             {
-                maxi=right[i]+left[i-1];
-                // cout<<maxi<<" "<<i<<endl;
-                ans=i;
+                ans=score;
+                ind=i;
             }
+            
         }
         
-        
-        return ans;
+        return ind+1;
     }
 };
