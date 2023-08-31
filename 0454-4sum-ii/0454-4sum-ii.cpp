943bc9e25c79320845f0e762be6e94ca9e0cfccb
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-08-31 08:00:31 +0530

    Time: 349 ms (16.99%), Space: 24.7 MB (30.24%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& a, vector<int>& b, vector<int>& c, vector<int>& d)    {
+        map<int,int> mp;
+        for(auto z:c){
+            for(auto w:d)
+            {
+                mp[w+z]++;
+            }
+        }
+        int ans=0;
+        for(auto x:a){
+            for(auto y:b){
+                    if(mp.find(-1*(x+y))!=mp.end()) ans+=mp[-(x+y)];
+                }
+        }
+        return ans;
+    }
+};
