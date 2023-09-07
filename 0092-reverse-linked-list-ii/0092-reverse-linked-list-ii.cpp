0d04738f2c4dc9040040610a1d2c156a0ab7e1b7
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-09-07 10:25:50 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (46.43%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        int cnt=0;
+        ListNode* temp=head;
+        while(temp) {cnt++;temp=temp->next;}
+        if(cnt<0) return head;
+        int val=right-left;
+        temp=head;
+        ListNode* prevvals=new ListNode(0);
+        ListNode* ans=prevvals;
+        left--;
+        while(left--){
+            prevvals->next=temp;
+            prevvals=temp;
+            temp=temp->next;
+        }
+        // cout<<prevvals->val;
+        ListNode* prev=NULL;
+        ListNode* vals=temp;
+        val++;
+        while(val&&temp){
+            ListNode* nex=temp->next;
+            val--;
+            temp->next=prev;
+            prev=temp;
+            temp=nex;
+        }
+        // cout<<prev->val;4if()
+        // if(prevvals!=head)
+        if(prevvals->next) prevvals->next=prev;
+        vals->next=temp;
+        if(prevvals->next) return ans->next;
+        return prev;
+        
+        
+        
+    }
+};
