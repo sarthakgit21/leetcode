1c78b21fae3a97098900d4a40a93fd1002b112f2
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-09-15 11:18:59 +0530

    Time: 1447 ms (14.93%), Space: 246.7 MB (9.32%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,52 +1,86 @@
 class Solution {
 public:
+    vector<int> parent;
+    vector<int> rank;
+    
+    int find (int x) {
+        if (x == parent[x]) 
+            return x;
+    
+        return parent[x] = find(parent[x]);
+    }
+    
+    void Union (int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+    
+        if (x_parent == y_parent) 
+            return;
+    
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
     int minCostConnectPoints(vector<vector<int>>& points) {
-        unordered_map<int,vector<pair<int,int>>> mp;
         
         int n=points.size();
         
+        parent.resize(n);
+        rank.resize(n);
+        
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j) continue;
-                int dis=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-                mp[i].push_back({j,dis});
-            }
+            parent[i]=i;
+            rank[i]=1;
         }
         
-//         for(auto x:mp){
-//             cout<<x.first<<" -";
-//             for(auto d:x.second){
-//                 cout<<d.first<<" "<<d.second<<" ,";
-//             }
-//             cout<<endl;
-//         }
-        
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
-        q.push({0,0});
         int ans=0;
         
-        vector<bool> vis(n,false);
-        while(!q.empty()){
-            int node=q.top().second;
-            int dis =q.top().first;
-            q.pop();
-            // cout<<dis<<endl;
-            if(vis[node]==true) continue;
-            vis[node]=true;
-            ans+=dis;
-            // cout<<node<< " "<<dis<<endl;
-            for(auto x:mp[node]){
-                int adjnode=x.first;
-                int dist=x.second;
-                if(vis[adjnode]==false){
-                    // cout<<adjnode<<endl;
-                    q.push({dist,adjnode});
-                }
+        vector<vector<int>> v;
+        
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+               int x1 = points[i][0];
+                int y1 = points[i][1];
                 
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                
+                int d = abs(x1-x2) + abs(y1-y2);
+                
+                
+                v.push_back({i, j, d});
             }
         }
         
-        return ans;
+        auto cmp=[&](vector<int> &a,vector<int>&b){
+            return a[2]<b[2];
+        };
         
+        sort(v.begin(),v.end(),cmp);
+        
+        return fun(v);
+        
+    }
+    
+    int fun(vector<vector<int>>&v){
+        int ans=0;
+        for(auto x:v){
+            int a=x[0];
+            int b=x[1];
+            int c=x[2];
+            
+            if(find(a)!=find(b)){
+                Union(a,b);
+                ans+=c;
+            }
+            
+        }
+        return ans;
     }
 };
