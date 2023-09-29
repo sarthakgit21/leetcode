c1db46fff716fd604a55da1819ce83d721f6de3d
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-09-29 11:24:53 +0530

    Time: 137 ms (14.11%), Space: 96.7 MB (29.47%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    bool isSroted(vector<int> nums){
-        for(int i=1;i<nums.size();i++){
-            if(nums[i-1]>nums[i]){
-                return false;
-            }
-        }
-        return true;
-    }
     bool isMonotonic(vector<int>& nums) {
-        vector<int> vals(nums);
-        reverse(vals.begin(),vals.end());
-        return isSroted(vals)|| isSroted(nums);
+        int n=nums.size();
+        bool left=false;
+        bool right=false;
+        
+        for(int i=1;i<n;i++){
+            
+            if(nums[i-1]<nums[i]) left=true;
+            else if(nums[i-1]>nums[i])right=true;
+            
+            if(left==true){
+                if(right==true) return false;
+            }
+            if(right==true){
+                if(left==true) return false;
+            }
+            
+        }
+        
+        return true;
     }
 };
