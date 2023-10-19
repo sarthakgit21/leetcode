d6ee7bd3bc260691bb52c76744c9675aa97299bc
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-10-19 06:55:50 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (39.46%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -14,8 +14,32 @@ public:
         s=ans;
     }
     bool backspaceCompare(string s, string t) {
-        fun(s);
-        fun(t);
-        return t==s;
+       int n=s.size();
+       int m=t.size();
+        int ind1=0,ind2=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='#'){
+                ind1--;
+                ind1=max(0,ind1);
+            }
+            else{
+                s[ind1++]=s[i];
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(t[i]=='#'){
+                ind2--;
+                ind2=max(0,ind2);
+            }
+            else{
+                t[ind2++]=t[i];
+            }
+        }
+        if(ind1!=ind2) return false;
+        // cout<<s<<t;
+        for(int i=0;i<ind1;i++){
+            if(s[i]!=t[i]) return false;
+        }
+        return true;
     }
 };
