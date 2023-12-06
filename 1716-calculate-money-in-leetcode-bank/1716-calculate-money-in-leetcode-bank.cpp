2e29d88ed3a30731e30e28b14983fbcdeabed195
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2023-12-06 08:12:50 +0530

    Time: 2 ms (49.74%), Space: 6.2 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -3,10 +3,14 @@ public:
     int totalMoney(int n) {
         int ans=0;
         int val=0;
-        while(n>7){
-            ans+=val*7+28;
-            val++;
-            n-=7;
+        if(n>7){
+            val=n/7;
+            // val--;
+            int x=(val-1)*(val)/2;
+            ans+=x*7+val*28;
+            // cout<<ans<<endl;
+            n%=7;
+            // val++;
         }
         ans+=(n*(n+1))/2;
         ans+=(val*n);
