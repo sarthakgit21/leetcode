fe780872417df495d66d5dd98770c4d4b4da3d0f
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2024-02-05 11:42:24 +0530

    Time: 58 ms (15.19%), Space: 13.1 MB (10.77%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<char,int> mp;
+        for(auto x:s) mp[x]++;
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
